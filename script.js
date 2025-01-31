2ba56269596a1d79b72f95717081b3834940c06a
Amgaa87 <amgaa6214@gmail.com>
2025-02-01 00:27:09 +0800

    Duusga

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 import { categories, alphabetLetters } from "./words.js";
-import { drawInitialStructure } from "./canvas.js";
+import {
+    drawBody,
+    drawHead,
+    drawInitialStructure,
+    drawLeftArm,
+    drawLeftLeg,
+    drawRightArm,
+    drawRightLeg,
+} from "./canvas.js";
 
 let chosenWord = "";
+let lettersGuessed = 0;
+let incorrectGuessesCount = 0;
 
 const categoryContainer = document.getElementById("category-container");
 const alphabetContainer = document.querySelector(".alphabet-container");
@@ -52,6 +62,61 @@ const createAlphabetButtons = () => {
         const button = document.createElement("button");
         button.className = "letter";
         button.textContent = letter;
+        button.addEventListener("click", selectLetter);
         alphabetContainer.appendChild(button);
     });
+};
+
+const selectLetter = (e) => {
+    const selectedLetter = e.target.textContent;
+    const chosenWordArray = chosenWord.split("");
+
+    if (chosenWordArray.includes(selectedLetter)) {
+        revealLetters(chosenWordArray, selectedLetter);
+
+        if (chosenWordArray.length === lettersGuessed) {
+            console.log("win");
+        }
+    } else {
+        incorrectGuessesCount++;
+        drawMan;
+        if (incorrectGuessesCount === 6) {
+        }
+    }
+
+    e.target.disabled = true;
+};
+
+const revealLetters = (chosenWordArray, selectedLetter) => {
+    const dashes = document.querySelectorAll(".dashes");
+
+    chosenWordArray.forEach((letter, index) => {
+        if (letter === selectedLetter) {
+            dashes[index].textconmtent = letter;
+        }
+    });
+    console.log(dashes);
+};
+const drawMan = () => {
+    const drawFunctions = [
+        drawHead,
+        drawBody,
+        drawLeftArm,
+        drawRightArm,
+        drawLeftLeg,
+        drawRightLeg,
+    ];
+    if (incorrectGuessesCount <= drawFunctions.length) {
+        drawFunctions[incorrectGuessesCount - 1];
+    }
+};
+
+const displayResult = (isWin) => {
+    const h2 = document.querySelector("#results-container h2");
+    h2.textContent = isWin ? "YOU WON" : "you lost";
+
+    const p = document.querySelector("#results-container p");
+    p.textContent = `the chossen word was ${chosenWord}`;
+
+    newGame;
 };
